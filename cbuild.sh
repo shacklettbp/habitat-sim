1d5f7a4a3bfc30b620cf99f75a09db124b7aa1a5
Brennan Shacklett <bpshacklett@gmail.com>
2020-02-26 10:00:13 -0800

    Switch to headless built for multi-gpu

--- cbuild.sh
+++ cbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CMAKE_ARGS="-DUSE_SYSTEM_ASSIMP=Yes -DCMAKE_CUDA_HOST_COMPILER=/usr/bin/gcc" python setup.py develop --with-cuda --bullet
+CMAKE_ARGS="-DUSE_SYSTEM_ASSIMP=Yes -DCMAKE_CUDA_HOST_COMPILER=/usr/bin/gcc" python setup.py develop --with-cuda --headless
